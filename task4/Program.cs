45909f7aa1c63a6e176a6076fcf108cfba75bb25
IT <spanyukhin@list.ru>
2023-12-28 20:44:42 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 54;
+int b = 65;
+int c = 16;
+int d = 37;
+int e = 8;
+int max = a;
+if (b > max){
+  max = b;
+}
+if (c > max){
+  max = c;
+}
+if (d > max){
+  max = d;
+}
+if (e > max){
+  max = e;
+}
+Console.WriteLine(max);
