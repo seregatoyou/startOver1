882ca59d9fee7dec92d9c8761c2b62c046036018
IT <spanyukhin@list.ru>
2024-01-11 14:26:56 +0300

    add lesson1:task5

--- task5/Program.cs
+++ task5/Program.cs
@@ -1,10 +1,12 @@
-﻿int j = 10;
+﻿int j = 100;
 for (int i = 1; i<=j;i++){
     Console.Write(i);
+    Console.Write(' ');
 }
 Console.WriteLine("");
 int c = 1;
 while (c<=j){
     Console.Write(c);
+    Console.Write(' ');
     c++;
 }
